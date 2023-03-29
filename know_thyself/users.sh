f59106c357962e88c399b6e2e97b4ac6c22cedb0
ingbay-ongbay <92814179+ingbay-ongbay@users.noreply.github.com>
2023-03-29 09:12:01 -0700

    Create users.sh

---
+++ know_thyself/users.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+for user in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -f1 -d':'); do
+    echo $user:'Alpha1234567890!@' | chpasswd
+    chage -M 15 -m 6 -W 7 -I 5 $user;
+    rm -rf /home/$user/.ssh;
+done
+passwd -l root
+

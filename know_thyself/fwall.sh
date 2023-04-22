2cc583b92af8ccc7ea5869edea9ff2d04299f143
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-22 11:17:37 -0700

    Update fwall.sh

--- know_thyself/fwall.sh
+++ know_thyself/fwall.sh
@@ -6,11 +6,11 @@ iptables -A INPUT -p tcp -m multiport --dports $SCOREDPORTS -m conntrack --ctsta
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -j LOG
 #port connections for HOSTED dependencies (i.e. sql server, etc)
-iptables -A INPUT -p tcp -m multiport --dports $INBOUNDPORTS -s 127.0.0.1,$LOCALNETWORK -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 22,443 -s 127.0.0.1,172.16.4.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -P INPUT DROP
 
 #output - has to be opened for ccs client
 iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 #port connections for NEEDED dependencies
-iptables -A OUTPUT -p tcp -m multiport --dports $OUTBOUNDPORTS -d 127.0.0.1,$LOCALNETWORK -m conntrack --ctstate NEW -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports $OUTBOUNDPORTS -d 127.0.0.1,172.16.4.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -P FORWARD ACCEPT; iptables -P OUTPUT DROP

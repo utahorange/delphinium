dc9ae11982dc28d052abe7e0b8c0fc5815572347
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-22 11:31:52 -0700

    Update users.sh

--- know_thyself/users.sh
+++ know_thyself/users.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for user in $(cat /etc/passwd | grep -E "/bin/.*sh" | cut -f1 -d':'); do
-    echo $user:'Alpha1234567890!@' | chpasswd
+    echo $user:'Monkepatriot123!@#' | chpasswd
     chage -M 15 -m 6 -W 7 -I 5 $user;
     rm -rf /home/$user/.ssh;
 done

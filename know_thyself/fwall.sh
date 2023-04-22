ea1d1fb6e0f63c41606000ae065b0ffc624e4073
prince-of-tennis <86171033+prince-of-tennis@users.noreply.github.com>
2023-04-22 11:20:54 -0700

    Update fwall.sh

--- know_thyself/fwall.sh
+++ know_thyself/fwall.sh
@@ -12,5 +12,5 @@ iptables -P INPUT DROP
 #output - has to be opened for ccs client
 iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 #port connections for NEEDED dependencies
-iptables -A OUTPUT -p tcp -m multiport --dports $OUTBOUNDPORTS -d 127.0.0.1,172.16.4.0/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 3306 -d 127.0.0.1,172.16.4.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -P FORWARD ACCEPT; iptables -P OUTPUT DROP
